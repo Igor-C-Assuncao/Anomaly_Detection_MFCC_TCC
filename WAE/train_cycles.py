6372a095b8c68a8ddbf0cf80bd63954ada292a87
nada <nada>
2025-04-11 18:13:04 -0300

    Train and preprossessing running

--- WAE/train_cycles.py
+++ WAE/train_cycles.py
@@ -58,10 +58,10 @@ def predict(model, test_tensors, tqdm_desc):
 
 
 def calculate_train_losses(model, args):
-
-    with open(f"{args.data_folder}final_train_tensors_{args.FEATS}.pkl", "rb") as tensor_pkl:
-        train_tensors = pkl.load(tensor_pkl)
-        train_tensors = [tensor.to(args.device) for tensor in train_tensors]
+    # *** CHANGE: Use the new train data path ***
+    with open(args.train_data_path, "rb") as tensor_pkl:
+        train_tensors_np = pkl.load(tensor_pkl)
+        train_tensors = [th.tensor(tensor_np, dtype=th.float32).to(args.device) for tensor_np in train_tensors_np]
 
     train_losses = predict(model, train_tensors, "Calculating training error distribution")
 
@@ -71,11 +71,13 @@ def calculate_train_losses(model, args):
 def offline_train(model, args):
 
     print(f"Starting offline training")
+    # *** CHANGE: Use the new train data path ***
+    print(f"Offline training: loading data from {args.train_data_path}")
+    with open(args.train_data_path, "rb") as tensor_pkl:
+        train_tensors_np = pkl.load(tensor_pkl)
+        train_tensors = [th.tensor(tensor_np, dtype=th.float32).to(args.device) for tensor_np in train_tensors_np]
 
-    with open(f"{args.data_folder}final_train_tensors_{args.FEATS}.pkl", "rb") as tensor_pkl:
-        train_tensors = pkl.load(tensor_pkl)
-        train_tensors = [tensor.to(args.device) for tensor in train_tensors]
-
+  
     model, loss_over_time = train_model(model,
                                         train_tensors,
                                         epochs=args.EPOCHS,
@@ -94,52 +96,66 @@ def offline_train(model, args):
 
 def calculate_test_losses(model, args):
 
-    with open(f"{args.data_folder}final_test_tensors_{args.FEATS}.pkl", "rb") as tensor_pkl:
-        test_tensors = pkl.load(tensor_pkl)
-        test_tensors = [tensor.to(args.device) for tensor in test_tensors]
+    # *** CHANGE: Use the new train data path ***
+    with open(args.test_data_path, "rb") as tensor_pkl:
+        test_tensors_np = pkl.load(tensor_pkl)
+        test_tensors = [th.tensor(tensor_np, dtype=th.float32).to(args.device) for tensor_np in test_tensors_np]
 
     test_losses = predict(model, test_tensors, "Testing on new data")
 
     losses_over_time = {"test": test_losses, "train": args.train_losses}
 
+    
     with open(args.results_string("complete"), "wb") as loss_file:
         pkl.dump(losses_over_time, loss_file)
 
     return model
 
 
+
 def load_parameters(arguments):
 
-    FEATS_TO_NUMBER = {"analog_feats": 8, "digital_feats": 8, "all_feats": 16}
+    FEATS_TO_NUMBER = {"analog_f6eats": 8, "digital_feats": 8, "all_feats": 313} 
 
     arguments.device = th.device('cuda' if th.cuda.is_available() else 'cpu')
 
     arguments.FEATS = f"{arguments.FEATS}_feats"
     arguments.NUMBER_FEATURES = FEATS_TO_NUMBER[arguments.FEATS]
 
+ 
     arguments.results_folder = "results/"
-    arguments.data_folder = "data/"
+    data_base_path = "Data/preprocessed_mimii/"
 
+
+    arguments.train_data_path = os.path.join(data_base_path, f"{arguments.machine_type}_{arguments.machine_id}_train_cycles.pkl")
+    arguments.test_data_path = os.path.join(data_base_path, f"{arguments.machine_type}_{arguments.machine_id}_test_cycles.pkl")
+    
+    #new 
+    if not os.path.exists(arguments.train_data_path): print(f"Warning: {arguments.train_data_path} not found")
+    if not os.path.exists(arguments.test_data_path): print(f"Warning: {arguments.test_data_path} not found")
+
+    
     if "tcn" in arguments.MODEL_NAME:
         arguments.model_string = f"{arguments.MODEL_NAME}_{arguments.FEATS}_{arguments.EMBEDDING}_{arguments.tcn_layers}_{arguments.tcn_hidden}_{arguments.tcn_kernel}"
     else:
+        
         arguments.model_string = f"{arguments.MODEL_NAME}_{arguments.FEATS}_{arguments.EMBEDDING}_{arguments.LSTM_LAYERS}"
 
-    print(f"Starting execution of model: {arguments.model_string}")
-
+   
+    os.makedirs(arguments.results_folder, exist_ok=True)
     arguments.results_string = lambda loop_no: f"{arguments.results_folder}final_{loop_no}_losses_{arguments.model_string}_{arguments.EPOCHS}_{arguments.LR}.pkl"
     arguments.model_saving_string = f"{arguments.results_folder}final_offline_{arguments.model_string}_{arguments.EPOCHS}_{arguments.LR}.pt"
 
     return arguments
 
-
 def main(arguments):
 
-    MODELS = {"lstm_ae": LSTM_AE, "lstm_sae": LSTM_SAE, 
+    MODELS = {"lstm_ae": LSTM_AE, "lstm_sae": LSTM_SAE,
               "multi_enc_sae": LSTM_SAE_MultiEncoder, "multi_enc_ae": LSTM_AE_MultiEncoder,
               "diff_comp_sae": LSTM_SAE_MultiComp, "diff_comp_ae": LSTM_AE_MultiComp,
               "tcn_ae": TCN_AE}
 
+  
     if "tcn" in arguments.MODEL_NAME:
         model = MODELS[arguments.MODEL_NAME](arguments.NUMBER_FEATURES,
                                              arguments.EMBEDDING,
@@ -149,6 +165,7 @@ def main(arguments):
                                              arguments.tcn_hidden,
                                              arguments.tcn_kernel).to(arguments.device)
     else:
+        
         model = MODELS[arguments.MODEL_NAME](arguments.NUMBER_FEATURES,
                                              arguments.EMBEDDING,
                                              arguments.DROPOUT,
@@ -157,16 +174,21 @@ def main(arguments):
                                              arguments.sparsity_weight,
                                              arguments.sparsity_parameter).to(arguments.device)
 
+   
     if os.path.exists(arguments.model_saving_string) and not arguments.force_training:
         model.load_state_dict(th.load(arguments.model_saving_string))
         arguments.train_losses = calculate_train_losses(model, arguments)
-    else:
+    else:           
         model, arguments.train_losses = offline_train(model, arguments)
 
+    
     calculate_test_losses(model, arguments)
+    print("Execution finished.")
 
 
-if __name__ == "__main__":
-    argument_dict = parse_arguments()
+
+
+if __name__ == "__main__":    
+    argument_dict = parse_arguments()    
     argument_dict = load_parameters(argument_dict)
     main(argument_dict)
